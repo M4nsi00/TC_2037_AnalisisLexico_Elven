d35d190dccfe5ef33f7b7ad71367d91e64b3dd12
M4nsi00 <sebastian.mansillacots@gmail.com>
2026-03-20 20:01:43 -0600

    Automata mejorado, ahora toma strings :0

--- Automata/Automata_elven.pl
+++ Automata/Automata_elven.pl
@@ -1,54 +1,55 @@
 %Author: Sebastian Mansilla Cots
 %Date: 11/03/2026
 %Project: Lexical Analysis Implementation (Elven language)
-%DFA
+%Purpose of the project: Lexical analysis parser that uses a DFA
+%to detect specific words of the elven language.
 
 
 % Initial State
 start(s).
 
 %Define transitions (where,to,with)
+
+%cor (wors for coron and cormarë)
 transition(s,q0,c).
 transition(q0,q1,o).
+transition(q1,q2,r).
 
 %coron
-transition(q1,q2,r).
 transition(q2,q3,o).
-transition(q3,q4,n).
+transition(q3,qf,n).
 
 %cormarë
-transition(q2,q5,m).
-transition(q5,q6,a).
-transition(q6,q7,r).
-transition(q7,q8,ë).
+transition(q2,q4,m).
+transition(q4,q5,a).
+transition(q5,q6,r).
+transition(q6,qf,ë).
 
 %craban
-transition(q0,q9,r).
+transition(q0,q7,r).
+transition(q7,q8,a).
+transition(q8,q9,b).
 transition(q9,q10,a).
-transition(q10,q11,b).
-transition(q11,q12,a).
-transition(q12,13,n).
+transition(q10,qf,n).
 
 %cú
-transition(q0,q14,ú).
+transition(q0,qf,ú).
 
 %cuivie
-transition(q0,q15,u).
-transition(q15,q16,i).
-transition(q16,q17,v).
-transition(q17,q18,i).
-transition(q18,q19,e).
+transition(q0,q11,u).
+transition(q11,q12,i).
+transition(q12,q13,v).
+transition(q13,q14,i).
+transition(q14,qf,e).
 
 %Final states
-final_state(q4).
-final_state(q8).
-final_state(q13).
-final_state(q14).
-final_state(q19).
+final_state(qf).
 
 %Automata
-is_elven(List) :-
- parse_list(Start, List).
+is_elven(Word) :-
+ atom_chars(Word, List),
+ start(S),
+ parse_list(S, List).
 
 %Base Case 1, the list is empty and the current state is a final_state
 parse_list(State, []) :-
@@ -64,3 +65,9 @@ parse_list(State, []) :-
 parse_list(State, [H|T]):-
  transition(State,X,H),
  parse_list(X,T).
+
+%False State, where the character doesn't have a state to go to, the word is consequently
+%NOT part of the language
+parse_list(State, [H|_]):-
+ \+ transition(State,H,_),
+ write('Is NOT a part of the Elven Language'), nl,!.
